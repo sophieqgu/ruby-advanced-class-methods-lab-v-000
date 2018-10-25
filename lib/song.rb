9c4502dee65e2389b76af3c4bf54ddfc0dcaed51
Sophie Gu <sophieqgu@gmail.com>
2018-10-25 16:02:02 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -9,5 +9,56 @@ class Song
   def save
     self.class.all << self
   end
-
+  
+  def self.create 
+    song = self.new 
+    self.all << song 
+    song
+  end 
+  
+  def self.new_by_name(name)
+    song = self.new
+    song.name = name
+    song 
+  end 
+  
+  def self.create_by_name(name)
+    song = self.new 
+    song.name = name 
+    @@all << song 
+    song 
+  end 
+  
+  def self.find_by_name(name)
+    @@all.find {|song| song.name == name}
+  end
+  
+  def self.find_or_create_by_name(name)
+    if self.find_by_name(name)
+      self.find_by_name(name)
+    else
+      self.create_by_name(name)
+    end
+  end
+  
+  def self.alphabetical
+    @@all.sort_by {|song| song.name}
+  end
+  
+  def self.new_from_filename(filename)
+    song = self.new
+    parsed = filename.split(" - ") 
+    song.artist_name = parsed[0]
+    song.name = parsed[1].slice(0..-5)
+    song 
+  end
+  
+  def self.create_from_filename(filename)
+    song = self.new_from_filename(filename)
+    @@all << song 
+  end 
+  
+  def self.destroy_all
+    @@all.clear 
+  end 
 end
